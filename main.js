43570679b6f021c8dd02d93893a7dea753407a69
GasparKral <147488938+GasparKral@users.noreply.github.com>
2023-10-24 13:21:14 +0200

    Base Idea

---
+++ main.js
@@ -0,0 +1,13 @@
+let screenWidth = window.innerWidth;
+const selection = document.getElementsByClassName("firstLook")[0];
+console.log(selection);
+
+window.addEventListener('resize', () => {
+    screenWidth = window.innerWidth;
+    if (screenWidth >= 1440) {
+
+        //selection. = "reverse";
+
+    }
+})
+
